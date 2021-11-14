7e6ac598fedd3cc899ad4cf458d7c4ca4cc0fd07
brotherSI <siyabonga23shabangu@gmail.com>
2021-11-14 14:05:40 +0200

    blog filter feature

--- pages/script-2.js
+++ pages/script-2.js
@@ -1,4 +1,4 @@
-document.getElementById('log-head-id').addEventListener("click", function() {
+document.getElementById('log-head-id').addEventListener("click", function () {
     document.getElementById('login-id').style.marginTop = "10rem";
     document.getElementById('log-sign-hr-id').style.transform = "translateX(0%)";
     document.getElementById('login-form-id').style.display = "block";
@@ -6,7 +6,7 @@ document.getElementById('log-head-id').addEventListener("click", function() {
     document.getElementById('registration-id').style.display = "none";
     document.getElementById('reg-title-id').style.display = "none";
 });
-document.getElementById('sign-head-id').addEventListener("click", function() {
+document.getElementById('sign-head-id').addEventListener("click", function () {
     document.getElementById('login-id').style.marginTop = "5rem";
     document.getElementById('log-sign-hr-id').style.transform = "translateX(100%)";
     document.getElementById('login-form-id').style.display = "none";
@@ -14,3 +14,4 @@ document.getElementById('sign-head-id').addEventListener("click", function() {
     document.getElementById('registration-id').style.display = "block";
     document.getElementById('reg-title-id').style.display = "block";
 });
+
--- pages/script.js
+++ pages/script.js
@@ -75,3 +75,25 @@ gsap.from(".team_img_wrapper img", {
     delay: 3,
     duration: 1,
 });
+
+
+//Isotope filter
+
+//init Isotope
+var $grid = $('.collection-list').isotope({
+    // options
+});
+// filter items on button click
+$('.filter-button-group').on('click', 'button', function () {
+    var filterValue = $(this).attr('data-filter');
+    resetFilterBtns();
+    $(this).addClass('active-filter-btn');
+    $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns() {
+    filterBtns.each(function () {
+        $(this).removeClass('active-filter-btn');
+    });
+}
